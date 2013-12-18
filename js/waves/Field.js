91c14eb9b41bc3de059d5855184c98a0d7e52601
bbrh <a.polovjan@gmail.com>
2013-12-18 14:45:13 +0100

    Update Field.js

--- js/waves/Field.js
+++ js/waves/Field.js
@@ -1,13 +1,13 @@
+/*global Field:true, Canvas, Size*/
+
 Field = function(objectPlaceholder) {
   this.objectPlaceholder = objectPlaceholder;
   var canvasPlaceholder =
     $('<div>', {class:'canvas-placeholder'}).appendTo(objectPlaceholder);
   this.canvas = new Canvas(canvasPlaceholder);
-
-}
+};
 
 Field.prototype.draw = function(sources) {
-  var at=0, bt=0, ct=0, dt=0, t;
   this.canvas.init();
 
   for (var x = 0; x < Size.w; x++) {
@@ -27,18 +27,18 @@ Field.prototype.waveFun = function(sources, point) {
     maxAmp += sources[si].amplitude;
     res += sources[si].amplitude *
       (1+Math.cos(
-        Field.distance(point, sources[si]) / 
-        Size.w*sources[si].wavelength*Math.PI));
+        Size.w * Field.distance(point, sources[si]) /
+        (40 * sources[si].wavelength * Math.PI)));
   }
 
   return Math.floor(128 * res / maxAmp);
-}
+};
 
 Field.distance = function(a, b) {
   var da = a.x - b.x;
   var dy = a.y - b.y;
   return Math.pow(da*da + dy*dy, 0.5);
-}
+};
 
 Field.prototype.cleanup = function () {
   this.objectPlaceholder.empty();
