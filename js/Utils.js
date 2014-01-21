a5cfa4c5ed6c5b5296cb6ede26ec3d1cfca1ccba
bbrh <a.polovjan@gmail.com>
2014-01-21 14:34:58 +0100

    Update Utils.js

--- js/Utils.js
+++ js/Utils.js
@@ -2,14 +2,23 @@
 
 Utils = {};
 
-Utils.getSize = function () {
-  var h = window.innerHeight -
-    $('.header').outerHeight() -
-    $('.footer').outerHeight();
-  var w = window.innerWidth;
-  return {
-    m : Math.min(h, w),
-    h : h,
-    w : w
+Utils.Size = function (w, h) {
+  this.w = w;
+  this.h = h;
+
+  this.m = Math.min(h, w);
+  this.center = {
+    x: w/2,
+    y: h/2
   };
 };
+
+Utils.getSize = function (factor) {
+  factor = factor || {x:1, y:1};
+  var h = Math.ceil((window.innerHeight -
+    $('.header').outerHeight() -
+    $('.footer').outerHeight()) / factor.y);
+  var w = Math.ceil(window.innerWidth / factor.x);
+  return new Utils.Size(w, h);
+};
+
