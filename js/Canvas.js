0f9087a29e94190e00fb26486144d277b2d4cefd
bbrh <a.polovjan@gmail.com>
2014-01-24 13:16:56 +0100

    Update Canvas.js

--- js/Canvas.js
+++ js/Canvas.js
@@ -1,16 +1,20 @@
-Canvas = function (container, opts) {
+/*global Canvas:true, Size*/
+
+Canvas = function (container, opts, size) {
   this.container = container;
-  this.init();
+  this.size = size || Size;
+  this.canvas = $('<canvas>')
+    .attr('width', this.size.w)
+    .attr('height', this.size.h)
+    .appendTo(this.container);
+
 };
 
 Canvas.prototype.init = function() {
-  if (this.canvas) this.canvas.remove();
-  this.canvas = $('<canvas>')
-    .attr('width', Size.w)
-    .attr('height', Size.h)
-    .appendTo(this.container);
+  this.cleanup();
+
   this.ctx = this.canvas[0].getContext('2d');
-  this.canvasData = this.ctx.getImageData(0, 0, Size.w, Size.h);
+  this.canvasData = this.ctx.getImageData(0, 0, this.size.w, this.size.h);
 };
 
 Canvas.prototype.done = function() {
@@ -21,12 +25,29 @@ Canvas.prototype.drawPixel = function (size, x, y, r, g, b, a) {
   for (var i=x*size; i<(x+1)*size; i++) {
     for (var j=y*size; j<(y+1)*size; j++) {
 
-      var index = (i + j * Size.w) * 4;
+      var index = (i + j * this.size.w) * 4;
 
       this.canvasData.data[index + 0] = r;
       this.canvasData.data[index + 1] = g;
       this.canvasData.data[index + 2] = b;
       this.canvasData.data[index + 3] = a;
     }
+  }
+};
+
+Canvas.prototype.cleanup = function () {
+  this.canvas[0].getContext('2d').clearRect (0, 0, this.size.w, this.size.h);
+};
+
+Canvas.prototype.setSize = function (newSize) {
+  if (newSize) {
+    this.cleanup();
+    this.size = newSize;
+    this.canvas
+      .attr('width', this.size.w)
+      .attr('height', this.size.h);
+    return this;
+  } else {
+    return this.size;
   }
 };
